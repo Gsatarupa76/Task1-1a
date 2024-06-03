1a71d70db54bc3938079d451d5be447b2273c549
Gsatarupa76 <gayatritym@gmail.com>
2024-06-03 14:21:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('project-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // TO DO: Implement form submission logic here
+    console.log('Form submitted!');
+});
